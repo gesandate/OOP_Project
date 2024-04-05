5139426d2e858f8963c278a4bb5d6087e158ab3e
sebas <sebastainlevario@gmail.com>
2024-04-05 16:30:09 -0600

    person class added

---
+++ Person.java
@@ -0,0 +1,58 @@
+public class Person {
+    private String FName;
+    private String LName;
+    private int Age;
+    private String gender;
+
+    public Person(String FName, String LName, int Age, String gender) {
+        this.FName = FName;
+        this.LName = LName;
+        this.Age = Age;
+        this.gender = gender;
+    }
+
+    // Getters and setters for the attributes
+    public String getFName() {
+        return FName;
+    }
+
+    public void setFName(String FName) {
+        this.FName = FName;
+    }
+
+    public String getLName() {
+        return LName;
+    }
+
+    public void setLName(String LName) {
+        this.LName = LName;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// -_-
