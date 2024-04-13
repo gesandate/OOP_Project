09a78f04d1c30c49d436e4c79389de1c475715f5
Gilberto Sandate <gsandate2@miners.utep.edu>
2024-04-13 01:42:26 -0600

    Began the adminRemove method, just need the file writing part

--- Admin.java
+++ Admin.java
@@ -1,3 +1,9 @@
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.Scanner;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
 /**
  * This is an admin person in the system
  * The admin class extends the person class adding the admin methods
@@ -20,15 +26,58 @@ public class Admin extends Person{
         
     }
 
+/**
+ * Adds a car to the car_data csv
+ * 
+ * @param car the car to be added
+ */
+public void adminAdd(Car car){}
 
 /** 
- * Does an admin action and logs it.
+ * Removes a car using ID and logs it.
  * 
- * @param action the admin action that is done.
+ * @param ID the car that is getting removed.
  */
-public void adminActon(String action){
+public void adminRemoveID(int ID){
 
-    log.logAction(this, action);
+    try{
+
+        //intitializes variables for reading and changing the csv
+        String line;
+        FileReader     fileReader     = new FileReader("car_data.csv");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        line = bufferedReader.readLine();
+        //String [] splitLine = line.split(line);
+        /*int index;
+        
+        for (int i = 0; i < splitLine.length; i++){
+
+            if (splitLine[i].equals("ID")){
+
+                index = i;
+
+            }
+
+        }
+        */
+
+        //traversing the file looking for the car id to remove
+        for (int i = 0; i < ID; i++){
+
+            line = bufferedReader.readLine();
+        
+        }
+
+        //once there, remove the car
+
+        //have to make a temp file?
+        //rewrite everything without that one car/line.
+
+    }catch (IOException err){
+
+        System.out.println("Error reading file lowkey");
+
+    }
 
 }
 }
